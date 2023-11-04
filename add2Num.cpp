f7e25d96b3c174e46c7d4c00fdf53c59b85d1a0e
sateesh <sateeshkumarp3113@gmail.com>
2023-11-04 09:27:21 +0530

    ISCP git practice commit 1

---
+++ add2Num.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int in1,in2;
+    cin>>in1>>in2;
+    cout<<"Sum = "<<in1+in2;
+    return 0;
+}
---
+++ add3Num.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int in1,in2,in3;
+    cin>>in1>>in2>>in3;
+    cout<<"Sum = "<<in1+in2+in3;
+    return 0;
+}
